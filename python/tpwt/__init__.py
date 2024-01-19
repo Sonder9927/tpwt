5b8732e187a13539373ca74d04580bdc4caef25b
Sonder Merak <merak19981999@gmail.com>
2024-01-19 17:18:44 +0800

    sac format (ch)

--- python/tpwt/__init__.py
+++ python/tpwt/__init__.py
@@ -1,6 +1,7 @@
 from tpwt import _lowlevel as rs
 from tpwt._lowlevel import hello
-from tpwt.flow import tpwt_run, evt_files
+from tpwt.flow import tpwt_run, evt_files, sac_format
+from tpwt.tpwt_flow.evt_make import head_from_sac
 
 
-__all__ = ["hello", "rs", "tpwt_run", "evt_files"]
+__all__ = ["hello", "rs", "tpwt_run", "evt_files", "head_from_sac", "sac_format"]
--- test/test_tpwt_flow.py
+++ test/test_tpwt_flow.py
@@ -1,11 +1,19 @@
-from tpwt import evt_files
+from tpwt import evt_files, sac_format, head_from_sac
 from tpwt import rs
 
 
 def test_evt_files(param):
     evt_files(param)
 
+def test_head1(param):
+    """
+    test extract and format
+    """
+    head_from_sac(param.target("sac"))
+    sac_format(param)
+
 
 if __name__ == "__main__":
     param = rs.load_param("param.jsonc")
-    test_evt_files(param)
+    # test_evt_files(param)
+    test_head1(param)
